a4191e0221b350a7f948c6d4858a267555211e55
Softvision-GeluHaiduc <51400741+Softvision-GeluHaiduc@users.noreply.github.com>
2021-07-01 23:19:19 +0300

    first maven commit

---
+++ src/test/java/tests/FirstTest.java
@@ -0,0 +1,16 @@
+package tests;
+
+import org.testng.annotations.Test;
+import utils.Driver;
+
+public class FirstTest {
+
+    public static Driver driver = null;
+
+    @Test
+    public void firstTest(){
+        driver = Driver.getInstance();
+        driver.navigate();
+        driver.exit();
+    }
+}
